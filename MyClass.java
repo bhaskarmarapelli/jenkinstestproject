cc59083621a70af65050a46f2116a9aaf03cee59
Dr. BHASKAR MARAPELLI <34471404+bhaskarmarapelli@users.noreply.github.com>
2024-01-04 12:29:05 +0530

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,13 @@
+class MyClass {
+  public static void main(String[] args) {
+    int x, y, sum;
+    
+    x = 100
+
+    
+    y = 200
+
+    sum = x + y;  // Calculate the sum of x + y
+    System.out.println("Sum is: " + sum); // Print the sum
+  }
+}
